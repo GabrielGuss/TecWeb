9088bbd308bf88fcca00ea1539a11c6796ecdebe
Cauan <devcauan@gmail.com>
2024-10-24 18:12:26 -0300

    feat: first commit

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from bs4 import BeautifulSoup
 
 BASE_URL = 'http://bibtranscolbpes.es.gov.br/mobile/'
 RESULT_URL = f'{BASE_URL}resultado.asp'
+
+def hello(nome):
+  return f'Hello, {nome}!'
+nome_user = input("Digite seu nome...")
+print(hello(nome_user))
